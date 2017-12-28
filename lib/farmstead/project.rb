d673cc8e42085d7dd4629f560c178966b40dba75
Ken <mastermindg@gmail.com>
2017-12-28 11:38:36 -0500

    More on scaffold

--- lib/farmstead/project.rb
+++ lib/farmstead/project.rb
@@ -11,33 +11,69 @@ module Farmstead
     attr_accessor :deploy
 
     def create
-      #create_directory
+      create_directory
       generate_files
+      start_deploy
     end
 
+    def start_deploy
+      Dir.chdir @name
+      deploy = system ("bash exec.sh")
+      p deploy.inspect
+    end
+
+    # Creates OR RE-Creates the Project Directory
     def create_directory
-      Dir.mkdir(@name) unless Dir.exist?(@name)
+      remove_dir(@name) if Dir.exist?(@name)
+      Dir.mkdir(@name)
     end
 
     # Generate from templates in scaffold
     def generate_files
-      scaffold = Dir.glob("scaffold/**", File::FNM_DOTMATCH) - %w[scaffold/. scaffold/..]
+      erbfiles = File.join("**", "*.erb")
+      scaffold = Dir.glob(erbfiles, File::FNM_DOTMATCH)
       scaffold.each do |file|
-        if File.file?(file)
-          p basename = File.basename(file, File.extname(file))
-          #template = File.read(file)
-          #results = ERB.new(template).result(binding)
-          #copy_to_directory(results, basename)
-        else
-          p basename = File.basename(file)
-          #Dir.mkdir("#{@name}/#{basename}")
+        file.gsub! /\bscaffold\b/, ""
+        foldername = File.dirname(file)
+        # Create folder structure of subdirectories
+        if foldername != "/"
+          create_recursive(foldername)
         end
+        projectpath = "#{@name}#{file}".chomp(".erb")
+        scaffoldpath = "scaffold#{file}"
+        template = File.read(scaffoldpath)
+        results = ERB.new(template).result(binding)
+        copy_to_directory(results, projectpath)
+      end
+    end
+
+    # Recursive Create
+    def create_recursive(path)
+      recursive = path.split("/")
+      directory = ""
+      recursive.each do |sub_directory|
+        directory += sub_directory + "/"
+        Dir.mkdir("#{@name}/#{directory}") unless (File.directory? directory)
+      end
+    end
+
+    # Recursive Remove
+    def remove_dir(path)
+      if File.directory?(path)
+        Dir.foreach(path) do |file|
+          if ((file.to_s != ".") && (file.to_s != ".."))
+            remove_dir("#{path}/#{file}")
+          end
+        end
+        Dir.delete(path)
+      else
+        File.delete(path)
       end
     end
 
     # Copies an ERB Template as a file to the destination directory
     def copy_to_directory(str, file_name)
-      open("#{@name}/#{file_name}", "a") do |f|
+      open(file_name, "a") do |f|
         f.puts str
       end
     end
