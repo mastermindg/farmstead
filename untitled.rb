16f254e6999f113dd2de3c59a4ca867f89b29b4c
Ken <mastermindg@gmail.com>
2017-12-28 14:40:50 -0500

    Removed old entries

--- untitled.rb
+++
@@ -1,9 +0,0 @@
-  #s.files = ["lib/farmstead.rb",
-            # "lib/farmstead/cowardlylion.rb",
-            # "lib/farmstead/gem_version.rb",
-            # "lib/farmstead/glenda.rb",
-            # "lib/farmstead/scarecrow.rb",
-            # "lib/farmstead/service.rb",
-            # "lib/farmstead/tinman.rb",
-            # "lib/farmstead/version.rb",
- #            "README.md"]
