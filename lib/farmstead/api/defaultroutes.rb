fa21a5a01faba07c782fee7c6f031f80a2ff7688
Ken <mastermindg@gmail.com>
2018-01-21 13:06:06 -0500

    Fix settings variable

--- lib/farmstead/api/defaultroutes.rb
+++ lib/farmstead/api/defaultroutes.rb
@@ -1,7 +1,7 @@
 module Sinatra
   module DefaultRoutes
     def self.registered(app)
-      app.set :views, settings.root + '/api/views'
+      app.set :views, app.settings.root + '/api/views'
       app.get "/" do
         "Welcome to our API"
       end
