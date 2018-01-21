f71fe27f55e2dc60bb7e3de91cd9b809d0118d7f
Ken <mastermindg@gmail.com>
2018-01-21 18:53:48 -0500

    Pull in MySQL Host into DB class

--- lib/farmstead/db.rb
+++ lib/farmstead/db.rb
@@ -2,8 +2,13 @@ require "sequel"
 
 module Farmstead
   class DB
+    def self.pull_variables
+      @@mysql_host = ENV["MYSQL_HOST"]
+    end
+    
     def self.add_source
-      "Test"
+      self.pull_variables
+      @@mysql_host
     end
   end
 end
