49e7ab002ff0abadfb2cd530b5ddb17766deeb80
Ken <mastermindg@gmail.com>
2018-01-29 01:45:19 -0500

    Troubleshoot Load Service Pt 3

--- lib/farmstead/pipeline/load.rb
+++ lib/farmstead/pipeline/load.rb
@@ -11,6 +11,18 @@ module Farmstead
   module Load
     class Service < Farmstead::Service
       def run
+        puts "Received: #{message.value}"
+          # Run the load method of the module referenced by the message
+          message = '{"module_name":"StopandShop","result":"Home - stop and shop"}'
+          obj = JSON.parse(message)
+          project_name = ENV["name"].capitalize
+          module_name = obj["module_name"]
+          my_module = Object.const_get "#{project_name}::#{module_name}"
+          puts obj["result"]
+          my_module::load(obj["result"])
+      end
+
+      def runs
         @consumer.subscribe("Road")
         trap('TERM') { @consumer.stop }
         @consumer.each_message do |message|
@@ -21,7 +33,7 @@ module Farmstead
           module_name = obj["module_name"]
           my_module = Object.const_get "#{project_name}::#{module_name}"
           puts obj["result"]
-          result = my_module::load(obj["result"])
+          my_module::load(obj["result"])
           @consumer.mark_message_as_processed(message)
         end
       end
